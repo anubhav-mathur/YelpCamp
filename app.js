ba8f78e38907df93cef5baf70ddbccfbb1d1eba1
Anubhav Mathur <anu1998.mat@gmail.com>
2019-01-15 16:37:26 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser:true});
+//mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser:true});
+mongoose.connect("mongodb://Anubhav:password123@ds157574.mlab.com:57574/yelp_camp", { useNewUrlParser: true });
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
